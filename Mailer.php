04d561b29590ae61f47c67f473ff0010568e280f
pawel.kania@e-msi.pl <pawel.kania@e-msi.pl>
2015-02-09 14:18:05 +0100

    poprawki

---
+++ Mailer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace itzen\mailer;
+
+use Yii;
+use itzen\setting\Setting;
+use yii\helpers\ArrayHelper;
+
+class Mailer extends \yii\swiftmailer\Mailer
+{
+    /**
+     * Overwrite mailer config using values from itzen/yii2-setting extension
+     */
+    public function init() {
+
+        $this->messageConfig = ArrayHelper::merge($this->messageConfig, [
+                'charset' => 'UTF-8',
+                'from'    => [Yii::$app->setting->get('adminEmail') => Yii::$app->setting->get('adminName')]
+            ]
+        );
+
+        $transport = [
+            'class'      => 'Swift_SmtpTransport',
+            'host'       => Yii::$app->setting->get('smtpHost'),
+            'username'   => Yii::$app->setting->get('smtpUser'),
+            'password'   => Yii::$app->setting->get('smtpPassword'),
+            'port'       => Yii::$app->setting->get('smtpPort'),
+            'encryption' => Yii::$app->setting->get('smtpEncryption'),
+        ];
+
+        $this->setTransport($transport);
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# yii2-mailer
+Mailer class. Using swiftmailer and smtp settings from database (itzen/yii2-setting)
